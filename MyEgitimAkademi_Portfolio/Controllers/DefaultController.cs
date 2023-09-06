2f9ca03fb782a407f9abdefd8563a95543cbb203
tugra <tugra@HOLYCRAB>
2023-09-06 20:49:05 +0300

    Add project files.

---
+++ MyEgitimAkademi_Portfolio/Controllers/DefaultController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyEgitimAkademi_Portfolio.Models;
+
+namespace MyEgitimAkademi_Portfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialQuickContact()
+        {
+            var values = db.QuickContact.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.Feature.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialService()
+        {
+            var values = db.Service.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.Skill.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAwardInfo()
+        {
+            var values = db.AwardInfo.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialAward()
+        {
+            var values = db.Awards.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialTestimonialInfo()
+        {
+            var values = db.TestimonialInfo.ToList();
+            return PartialView(values);
+        }
+
+
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = db.Testimonial.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialClientInfo()
+        {
+            var values = db.ClientInfo.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialClientStats()
+        {
+            var values = db.ClientStats.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialClients()
+        {
+            var values = db.Clients.ToList();
+            return PartialView(values);
+        }
+
+        [HttpGet]
+        public PartialViewResult PartialContactInfo()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult PartialContactInfo(Contact contact)
+        {
+            db.Contact.Add(contact);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+        public PartialViewResult PartialContact()
+        {
+            var values = db.Address.ToList();
+            return PartialView(values);
+        }
+    }
+}
---
+++ MyEgitimAkademi_Portfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyEgitimAkademi_Portfolio.Models;
+
+namespace MyEgitimAkademi_Portfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestimonialInfo()
+        {
+            var values = db.TestimonialInfo.ToList();
+            return View(values);
+        }
+
+        public ActionResult Testimonial()
+        {
+            var values = db.Testimonial.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddTestimonial(Testimonial testimonial)
+        {
+            db.Testimonial.Add(testimonial);
+            db.SaveChanges();
+            return RedirectToAction("Testimonial");
+        }
+
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var value = db.Testimonial.Find(id);
+            db.Testimonial.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Testimonial");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var value = db.TestimonialInfo.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            var value = db.Testimonial.Find(testimonial.TestimonialID);
+            value.NameSurname = testimonial.NameSurname;
+            value.ImageUrl = testimonial.ImageUrl;
+            value.Comment = testimonial.Comment;
+            value.Title = testimonial.Title;
+            db.SaveChanges();
+            return RedirectToAction("Testimonial");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateTestimonialInfo(int id)
+        {
+            var value = db.TestimonialInfo.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonialInfo(TestimonialInfo info)
+        {
+            var value = db.TestimonialInfo.Find(info.TestimonialID);
+            value.SubjectText = info.SubjectText;
+            db.SaveChanges();
+            return RedirectToAction("TestimonialInfo");
+
+        }
+    }
+}
